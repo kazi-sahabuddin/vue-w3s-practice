65dc63d60e39e95754de8530fe61322ee554589a
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2025-10-20 11:42:17 +0600

    v-for components

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,8 @@
   <h1>Food</h1>
   <div id="wrapper">
     <food-item 
-      food-name="Apples"
-      food-desc="Apples are a type of fruit that grow on trees."/>
-    <food-item 
-      food-name="Pizza"
-      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
-    <food-item 
-      food-name="Rice"
-      food-desc= "Rice is a type of grain that people like to eat."
-    />
+      v-for = "x in foods"
+      v-bind:food-name="x"/>
   </div>
 
 </template>
--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,32 +1,12 @@
 <template>
-    <!-- <div v-on:click="countClicks"> -->
-        <div>
-        <h2> {{ foodName }}</h2>
-         <p> {{ foodDesc }}</p>
-        <!--<p id = "red"> You have clicked me {{ clicks }} times.</p> -->
-    </div>
+   <div>
+    <h2>{{ foodName }}</h2>
+   </div>
 </template>
 
 <script>
 export default{
-    // data() {
-    //     return {
-    //         name: 'Apples',
-    //         message: 'I like apples',
-    //         clicks: 0
-
-    //     };
-    // },
-    // methods: {
-    //     countClicks(){
-    //         this.clicks++;
-    //     }
-    // }
-
-    props: [
-        'foodName',
-        'foodDesc'
-    ]
+    props: ['foodName']
 };
 </script>
 
