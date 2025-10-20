8b3518785bc07172a16de2d92404d558695817cf
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2025-10-20 11:26:37 +0600

    initail commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item 
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow on trees."/>
+    <food-item 
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
+    <food-item 
+      food-name="Rice"
+      food-desc= "Rice is a type of grain that people like to eat."
+    />
+  </div>
+
+</template>
+
+<script>
+
+export default{
+  data() {
+    return {
+      foods: ['Apples', 'Pizza', 'Rice', 'Fish','Cake']
+    };
+  }
+}
+
+</script>
+
+<style>
+
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#wrapper > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+}
+
+#wrapper > div:hover {
+  cursor: pointer;
+}
+
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- <div v-on:click="countClicks"> -->
+        <div>
+        <h2> {{ foodName }}</h2>
+         <p> {{ foodDesc }}</p>
+        <!--<p id = "red"> You have clicked me {{ clicks }} times.</p> -->
+    </div>
+</template>
+
+<script>
+export default{
+    // data() {
+    //     return {
+    //         name: 'Apples',
+    //         message: 'I like apples',
+    //         clicks: 0
+
+    //     };
+    // },
+    // methods: {
+    //     countClicks(){
+    //         this.clicks++;
+    //     }
+    // }
+
+    props: [
+        'foodName',
+        'foodDesc'
+    ]
+};
+</script>
+
+<style>
+
+#red{
+    font-weight: bold;
+    color: rgb(144, 12, 12);
+}
+
+</style>
