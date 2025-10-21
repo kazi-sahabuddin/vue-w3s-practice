aa2c4cbcdddf2f0387e30609b4754a32cd424b22
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2025-10-21 23:54:15 +0600

    feat: slots as card

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
-  <div>
-    <h3>Slot in Vue</h3>
-    <p>We send 'Hello World!' as content to the slot tag inside the SlotComp.vue component from App.vue.</p>
-    <slot-comp>Hello world</slot-comp>
+ <h3>Slots in vue</h3>
+ <p>We create card-like div boxes from the foods array.</p>
+  <div id="wrapper">
+    <slot-comp v-for="x in foods">
+      <img :src="x.url" :alt="x.name">
+      <h4>{{ x.name }}</h4>
+      <p>{{ x.desc }}</p>
+    </slot-comp>
   </div>
 </template>
-<script></script>
+
+<script>
+  export default {
+    data() {
+      return {
+        foods: [
+          {name: 'Apple', desc: 'Apples are a type of fruit that grow on trees.', url:'/assets/images/img_apple.svg'},
+          {name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', url:'/assets/images/img_pizza.svg'},
+          {name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', url:'/assets/images/img_rice.svg'},
+          {name: 'Fish', desc: 'Fish is an animal that lives in water.', url:'/assets/images/img_fish.svg'},
+          {name: 'Cake', desc: 'Cake is something sweet that tates good but is not consodered healthy.', url:'/assets/images/img_cake.svg'},
+        ]
+
+      };
+    }
+  }
+</script>
+
 <style>
-  p {
-    width: 200px;
-  }
-  #app div {
-    border: dashed black 1px;
-    margin: 10px;
-    padding: 10px;
-    display: inline-block;
-  }
+ #wrapper {
+  display: flex;
+  flex-wrap: wrap;
+ }
+
+ #wrapper img {
+  display: block;
+  margin: auto;
+  width: 60%;
+ }
 </style>
